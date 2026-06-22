977a133d89a4a319306e7f5e729f8b2ca31a8038
Kerry Clements <kerryclements@me.com>
2026-06-22 11:12:19 +0100

    chore: set up folder structure and routing

---
+++ src/pages/CoinDetail/CoinDetail.tsx
@@ -0,0 +1,9 @@
+const CoinDetail = () => {
+  return (
+    <main>
+      <h1>Coin Detail</h1>
+    </main>
+  );
+};
+
+export default CoinDetail;
---
+++ src/pages/MarketOverview/MarketOverview.tsx
@@ -0,0 +1,9 @@
+const MarketOverview = () => {
+  return (
+    <main>
+      <h1>Market Overview</h1>
+    </main>
+  );
+};
+
+export default MarketOverview;
